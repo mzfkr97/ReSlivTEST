13ba6c9c4691bdd5708f260b90dbc7053dc9d1a6
Roman <mzfkr97@gmail.com>
2021-01-12 09:24:46 +0300

    update

--- app/src/main/java/com/example/reslivtest/MainActivity.kt
+++ app/src/main/java/com/example/reslivtest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.reslivtest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -11,16 +12,14 @@ import com.example.reslivtest.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    private val actionMain = "com.example.reslivtest.ACTION_MAIN"
-    private val actionCity = "com.example.reslivtest.ACTION_CITY"
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home,
@@ -31,22 +30,6 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navView.setupWithNavController(navController)
 
-        shortCutsIntents()
-
-    }
-
-    private fun shortCutsIntents() {
-        val action = intent.action
-        action.let {
-            when (it) {
-                actionMain -> {
-                    findNavController(R.id.nav_host_fragment).navigate(R.id.navigation_home)
-                }
-                actionCity -> {
-                    findNavController(R.id.nav_host_fragment).navigate(R.id.navigation_city)
-                }
-            }
-        }
     }
 
 }
